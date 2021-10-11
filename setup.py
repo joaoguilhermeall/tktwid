515c66d83f46d346f6ff9cb66e6c5e2f914c8300
Joao Guiherme Costa <joaoguilhermedev@gmail.com>
2021-10-11 11:09:24 -0300

    Change module name and substituied 'is' by '=='

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open("README.md") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="widgets-theme-tkinter", # Replace with your own username
+    name="tkinter-theme-widgets",
     version=VERSION,
     author="Joao Guilherme Dev",
     author_email="joaoguilhermedev@gmail.com",
     description="Edited tkinter widgets to simplify the creation and assembly of a graphical interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(exclude=['./env', './tests', './dist']),
+    packages=setuptools.find_packages(exclude=['./env', './tests', './dist', './build']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- widgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from widgets.widgets import *
-
-if __name__ == "__main__":
-    pass
